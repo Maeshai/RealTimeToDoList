70a2b036ac46c2d244e03557e60e4ea0f73f68f2
Chaimaa El Ghandori <elghandori.chaimaa@gmail.com>
2024-09-22 11:00:46 +0000

    Add Task model

---
+++ models/Task.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const TaskSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    completed: {
+        type: Boolean,
+        default: false,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    }
+});
+
+module.exports = mongoose.model('Task', TaskSchema);
